94dd121e3231a39bbca26008ccb7eedef680d8af
Rajjj05 <bhavyarajs0569@gmail.com>
2025-07-07 02:11:50 +0530

    Last Final Commit

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
+const dotenv = require("dotenv");
+dotenv.config();
 
 passport.use(
   new GoogleStrategy(
